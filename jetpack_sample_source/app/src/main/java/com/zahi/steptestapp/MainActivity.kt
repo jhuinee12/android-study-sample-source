89df35a804f6db41b1917174407c55a10e677895
jhuinee12 <jhuinee12@gmail.com>
2022-06-08 15:16:25 +0900

    안드로이드 sensor 사용

--- jetpack_sample_source/app/src/main/java/com/zahi/steptestapp/MainActivity.kt
+++ jetpack_sample_source/app/src/main/java/com/zahi/steptestapp/MainActivity.kt
@@ -1,19 +1,20 @@
 package com.zahi.steptestapp
 
+import android.content.Context
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.zahi.steptestapp.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SensorEventListener {
 
     private lateinit var binding: ActivityMainBinding
+
+    lateinit var sensorManager: SensorManager
+    lateinit var sensor: Sensor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +22,31 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // 센서 연결
+        sensorManager = this.getSystemService(Context.SENSOR_SERVICE) as SensorManager
+        sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER)
+
+        // 센서 속도 설정
+        // SENSOR_DELAY_FASTEST : 0ms 최대한 빠르게
+        // SENSOR_DELAY_GAME : 20,000ms 게임에 적합한 속도
+        // SENSOR_DELAY_UI : 60,000ms UI 수정에 적합한 속도
+        // SENSOR_DELAY_NORMAL : 200,000ms 화면 방향 변화를 모니터링하기에 적합한 속도
+        sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
 
     }
+
+    override fun onSensorChanged(event: SensorEvent) {
+        if (event.sensor.type == Sensor.TYPE_STEP_COUNTER) {
+            binding.stepCount.text = event.values[0].toString()
+        }
+    }
+
+    override fun onAccuracyChanged(p0: Sensor?, p1: Int) {
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        sensorManager.unregisterListener(this)
+    }
+
 }
