46b1a7c77d95503ba27a076e3f3ff09c717fb99f
jhuinee12 <jhuinee12@gmail.com>
2022-08-14 16:35:02 +0900

    [DEV-6] Silence Push 기본 프로젝트 생성

---
+++ firebase_google_sample_source/silence_push/app/src/main/java/com/zahi/silence_push/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.zahi.silence_push
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
